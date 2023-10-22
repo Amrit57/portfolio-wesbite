6884225bf123dd41773ae6f4f08b13c41af8c4c1
Amrit57 <fenpadorje7@gmail.com>
2023-10-22 16:14:56 +0545

    Added links and profile picture.

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import ProfilePic from "@/public/E-68860.jpg";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src={ProfilePic}
               alt="Ricardo portrait"
               width="192"
               height="192"
@@ -62,11 +63,15 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a
-        <span className="font-bold">full-stack developer</span> with
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I&apos;m Amrit.</span> I&apos;m a
+        <span className="font-bold"> Front-end developer</span> with a passion
+        for creating
+        <span className="italic">
+          {" "}
+          user-friendly, visually appealing websites.
+        </span>{" "}
+        My perosnal preference is
+        <span className="underline"> Reactjs</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +105,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/amrit-lama-syangbo-ab50a5266/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +113,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/Amrit57"
           target="_blank"
         >
           <FaGithubSquare />
