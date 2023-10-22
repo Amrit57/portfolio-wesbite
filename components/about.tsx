ba26a68488e73ae7d5bd9bc8d6e4688b2dcb23c6
Amrit57 <fenpadorje7@gmail.com>
2023-10-22 16:12:28 +0545

    Added details and framer-motion animation.

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,37 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned
-        <span className="font-medium">full-stack web development</span>.
-        <span className="italic">My favorite part of programming</span> is the
+        After graduating
+        <span className="font-medium"> Highschool</span>, I decided to pursue my
+        passion for programming. While, I was learning through youtube, I got to
+        know about the excellent website called
+        <span className="font-medium"> The Odin Project</span> and followed the
+        Full-stack Javascript roadmap. After completing the{" "}
+        <span>Front-end section with React.</span> Now I&apos;m currently
+        learning
+        <span className="font-medium"> the full-stack web course</span>.
+        <span className="italic"> My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          {" "}
+          HTML, CSS, Javascript, Bootsrap and React
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with Figma and Tailwind CSS. I am always looking to
         learn new technologies. I am currently looking for a
-        <span className="font-medium">full-time position</span> as a software
+        <span className="font-medium"> full-time position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I&apos;m not coding</span>, I enjoy
+        playing games, watching movies, and youtube videos about the lastest
+        Tech and updates in software field . I also enjoy
+        <span className="font-medium"> learning new things</span>. I am
+        currently learning
+        <span className="font-medium"> Typescript and Framer-Motion</span>. I
+        also prefer playing football and boxing.
       </p>
     </motion.section>
   );
