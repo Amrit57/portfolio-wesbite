350607e3cf38759a4cd771440d1b9e87571d27f4
Amrit <fenpadorje7@gmail.com>
2024-01-16 10:10:48 +0545

    Refacored Images and Project info.

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import CV from "@/public/CV Builder.png";
+import TextUtils from "@/public/TextUtils.png";
+import WeatherApp from "@/public/Weather App.png";
 
 export const links = [
   {
@@ -60,30 +60,29 @@ export const projectsData = [
   {
     title: "CV Builder",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "This website helps you build your CV, Load exmaple resume and several other features. ",
     tags: ["React", "Vite.js", "CSS", "Material Design Icon", "Netlify"],
-    imageUrl: corpcommentImg,
+    imageUrl: CV,
     link: "https://delicate-choux-6f87e5.netlify.app/",
-    code: "https://github.com/Amrit57/CV-Application"
+    code: "https://github.com/Amrit57/CV-Application",
   },
   {
     title: "Weather App",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+      "This is a weather App which shows the weather of any given city in the world by handling Weather Api",
     tags: ["HTML", "JavaScript", "CSS", "Weather Api", "Github Pages"],
-    imageUrl: rmtdevImg,
+    imageUrl: WeatherApp,
     link: "https://amrit57.github.io/Weather-App/",
-    code: "https://github.com/Amrit57/Weather-App"
+    code: "https://github.com/Amrit57/Weather-App",
   },
   {
     title: "TextUtils",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "A public web app for quick analytics on text. It shows word count, character count, alert feedbacks, preview section and many more features.",
     tags: ["React", "Bootsrap 5", "Netlify"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: TextUtils,
     link: "https://textutlis-fb8214.netlify.app/",
-    code: "https://github.com/Amrit57/TextUtlis-App"
-
+    code: "https://github.com/Amrit57/TextUtlis-App",
   },
 ] as const;
 
