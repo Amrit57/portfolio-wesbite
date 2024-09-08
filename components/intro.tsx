2df857fc236158541bcf205128af0a827cd7bcee
Amrit <fenpadorje7@gmail.com>
2024-09-08 22:31:44 +0545

    CV update

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,7 +96,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="./Amrit Lama Resume(SE).pdf"
+          href="./Amrit Lama Syangbo_Resume.pdf"
           download
         >
           Download CV
